cd53ac0af739a6d7c1dc83bacb883e183ddc8132
Alberto Arroyo Raygada <albertoarroyo@belcorp.biz>
2024-06-17 11:42:25 -0500

    Refactoring

--- src/MyProjects.Application/Release/UseCases/Release/Commands/CreateReleaseCommand.cs
+++ src/MyProjects.Application/Release/UseCases/Release/Commands/CreateReleaseCommand.cs
@@ -5,14 +5,12 @@ namespace MyProjects.Application.Release.UseCases.Release.Commands
     public class CreateReleaseCommand : IRequest
     {
         public string Title { get; set; } 
-        public string? Description { get; set; }
-        public string? Owner { get; set; }
+        public string Description { get; set; }
 
-        public CreateReleaseCommand(string title, string? description = "", string owner = "default")
+        public CreateReleaseCommand(string title, string description)
         {
             Title = title;
-            Description = description;
-            Owner = owner;
+            Description = description;           
         }
     }
 }
--- src/MyProjects.Application/Release/UseCases/Release/Commands/CreateReleaseCommandHandler.cs
+++ src/MyProjects.Application/Release/UseCases/Release/Commands/CreateReleaseCommandHandler.cs
@@ -1,13 +1,37 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using Ddd.ValueObjects;
+using MediatR;
+using MyProjects.Domain.ReleaseAggregate;
 
 
 namespace MyProjects.Application.Release.UseCases.Release.Commands
 {
     public class CreateReleaseCommandHandler : IRequestHandler<CreateReleaseCommand>
     {
+        private readonly IReleasesRepository repository;
+        private readonly IMapper mapper;
+
+        public CreateReleaseCommandHandler(IReleasesRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
         public Task Handle(CreateReleaseCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var release = ReleaseEntity.Create(StringValueObject.Create(request.Title),
+                                               StringValueObject.Create(request.Description));
+
+            release.Validate(release);
+
+            if (!release.IsValid)
+            {
+                throw new Exception(release.GetBrokenRules().ToString());
+            }
+
+            repository.Create(release);
+
+            return Task.CompletedTask;
         }
     }
 }
